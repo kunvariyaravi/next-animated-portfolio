76128ae1000779acad04bf7bfb0dc6cb3ed60b49
kunvariyaravi <88199763+kunvariyaravi@users.noreply.github.com>
2024-07-09 17:41:59 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import Link from "next/link";
 import { motion } from "framer-motion";
 
 const Homepage = () => {
@@ -31,12 +32,16 @@ const Homepage = () => {
           </p>
           {/* BUTTONS */}
           <div className="w-full flex gap-4">
-            <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
-              View My Work
-            </button>
-            <button className="p-4 rounded-lg ring-1 ring-black">
-              Contact Me
-            </button>
+            <Link href="https://www.ravibarot.com/portfolio">
+              <a className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+                View My Work
+              </a>
+            </Link>
+            <Link href="https://www.ravibarot.com/contact">
+              <a className="p-4 rounded-lg ring-1 ring-black">
+                Contact Me
+              </a>
+            </Link>
           </div>
         </div>
       </div>
