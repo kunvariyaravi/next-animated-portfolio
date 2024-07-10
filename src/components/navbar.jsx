3cc6fd6e28cdc7bde68cfd80096af00dad394f62
kunvariyaravi <88199763+kunvariyaravi@users.noreply.github.com>
2024-07-10 15:22:28 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -90,16 +90,16 @@ const Navbar = () => {
       </div>
       {/* SOCIAL */}
       <div className="hidden md:flex gap-4 w-1/3">
-        <Link href="/">
+        <Link href="https://github.com/kunvariyaravi">
           <Image src="/github.png" alt="" width={24} height={24} />
         </Link>
-        <Link href="/">
+        <Link href="https://www.instagram.com/kunvariya.ravi/">
           <Image src="/instagram.png" alt="" width={24} height={24} />
         </Link>
-        <Link href="/">
+        <Link href="https://www.facebook.com/kunvariyaravi/">
           <Image src="/facebook.png" alt="" width={24} height={24} />
         </Link>
-        <Link href="/">
+        <Link href="https://www.linkedin.com/in/ravi-kunvariya-b35493201/">
           <Image src="/linkedin.png" alt="" width={24} height={24} />
         </Link>
       </div>
